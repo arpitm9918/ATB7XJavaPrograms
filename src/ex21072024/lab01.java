4d0c03b97362746777b626cc5c71e801d610c643
Arpit Mishra <arpitm041@gmail.com>
2024-07-21 13:01:23 +0530

    All Progrms

---
+++ src/ex21072024/lab01.java
@@ -0,0 +1,11 @@
+package ex21072024;
+
+public class lab01 {
+    public static void main(String[] args) {
+        for (int a=0; a <=10; a++)
+        {
+        System.out.println("Number is--"+ a);
+        //supported data type in for loop int, char, float, double, long, byte
+        }
+    }
+}
